dbcffb34d43e26f5991f59105c8f75ef5f846788
mixplusads-ar <mixplus.ads.ar@gmail.com>
2025-11-15 11:53:29 +0330

    Create social.ts

---
+++ src/data/social.ts
@@ -0,0 +1,7 @@
+export const social = [
+  { id: 'instagram', label: 'Instagram', href: 'https://instagram.com/...' },
+  { id: 'aparat', label: 'Aparat', href: 'https://www.aparat.com/...' },
+  { id: 'youtube', label: 'YouTube', href: 'https://youtube.com/...' },
+  { id: 'linkedin', label: 'LinkedIn', href: 'https://linkedin.com/...' },
+  { id: 'telegram', label: 'Telegram', href: 'https://t.me/...' }
+];
